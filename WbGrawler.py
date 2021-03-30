0df29b1e8ea756e60bcd82b67b4a2552662d8ed1
曾涛 <37970196+zengtao614@users.noreply.github.com>
2021-03-30 15:44:43 +0800

    Update WbGrawler.py
    判断页面无微博时,避免报错

--- WbGrawler.py
+++ WbGrawler.py
@@ -1,21 +1,21 @@
 """
-
 """
 
 import requests
 import logging
 import threadpool
 
+
 class WbGrawler():
-    def __init__(self,start=1,end=500):
+    def __init__(self, start=1, end=500):
         """
         参数的初始化
         :return:
         """
-        self.baseurl = "https://m.weibo.cn/api/container/getIndex?containerid=2304131792328230&"
+        self.baseurl = "https://m.weibo.cn/api/container/getIndex?containerid=1076033591355593&"
         self.headers = {
             "Host": "m.weibo.cn",
-            "Referer": "https://m.weibo.cn/p/2304131792328230",
+            "Referer": "https://m.weibo.cn/p/1076033591355593",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36",
             "X-Requested-with": "XMLHttpRequest"
         }
@@ -35,11 +35,14 @@ class WbGrawler():
         url = self.baseurl + "page=%d" % page
         try:
             response = requests.get(url, self.headers)
-            if response.status_code == 200:
+            #2021-3-30 此处加一个判断，ok为1时表示此页有微博，否则无微博
+            if response.status_code == 200 and response.json().get("ok") == 1:
                 return response.json()
+            else :
+                print("第%d页无微博"%page)
         except requests.ConnectionError as e:
             print("error")
-            self.logger.error("error",e.args)
+            self.logger.error("error", e.args)
 
     def parserJson(self, json):
         """
@@ -60,7 +63,7 @@ class WbGrawler():
                     picList.append(pic_dict)
             yield picList
 
-    def imgDownload(self,results):
+    def imgDownload(self, results):
         """
         下载图片
         :param results:
@@ -71,22 +74,23 @@ class WbGrawler():
                 img_name = img_dict.get("pid") + ".jpg"
                 try:
                     img_data = requests.get(img_dict.get("url")).content
-                    with open(self.path+img_name,"wb") as file:
+                    with open(self.path + img_name, "wb") as file:
                         file.write(img_data)
                         file.close()
-                        print(img_name+"\tdownload successed!")
+                        print(img_name + "\tdownload successed!")
                 except Exception as e:
-                    self.logger.error(img_name+"\tdownload failed!",e.args)
+                    self.logger.error(img_name + "\tdownload failed!", e.args)
 
-    def startCrawler(self,page):
+    def startCrawler(self, page):
         page_json = self.getPageJson(page)
-        results = self.parserJson(page_json)
-        self.imgDownload(results)
+        if page_json != None:
+            results = self.parserJson(page_json)
+            self.imgDownload(results)
 
 
 if __name__ == '__main__':
-    wg = WbGrawler(100,200)
+    wg = WbGrawler(1, 10)
     pool = threadpool.ThreadPool(10)
-    reqs = threadpool.makeRequests(wg.startCrawler,range(wg.start_pages,wg.end_pages))
+    reqs = threadpool.makeRequests(wg.startCrawler, range(wg.start_pages, wg.end_pages+1))
     [pool.putRequest(req) for req in reqs]
     pool.wait()
